439ab4f6aeb19f1459cfd278028cc4eb69963fab
iLioh <legonardo.garcia@gmail.com>
2025-05-22 17:50:20 -0500

    Add another test

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,9 @@
-const {add} = require('./index');
+const { add } = require('./index');
+
 test('adds 1 + 2 to equal 3', () => {
-expect (add (1, 2)).toBe(3) ;
+  expect(add(1, 2)).toBe(3);
+});
+
+test('adds 5 + 7 to equal 12', () => {
+  expect(add(5, 7)).toBe(12);
 });
